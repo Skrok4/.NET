c8a5a343c9587a3fb68b4bc0a96515f8baac5c2a
Shevchenko Oleksandr <73699809+Skrok4@users.noreply.github.com>
2023-04-28 01:32:55 +0300

    Add Swagger to the project for better API documentation and testing

---
+++ PPPI_№6/WebAPI/Controllers/WeatherForecastController.cs
@@ -0,0 +1,70 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetWeatherForecast")]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+        [HttpGet("GetNumberOfForecasts/{amount}")]
+        public IEnumerable<WeatherForecast> GetAmount(int amount)
+        {
+            return Enumerable.Range(1, amount).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+        [HttpGet("GetWeatherForecast/{id}")]
+        public ActionResult<WeatherForecast> GetById(int id)
+        {
+            var forecasts = Enumerable.Range(1, 20).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToList();
+
+            if (id < 0 || id >= forecasts.Count)
+            {
+                return NotFound();
+            }
+
+            return forecasts[id];
+        }
+
+        [HttpPost]
+        public IActionResult AddForecast([FromBody] WeatherForecast newForecast)
+        {
+            return Ok();
+        }
+    }
+}
---
+++ PPPI_№6/WebAPI/Startup.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerUI;
+
+namespace WebAPI
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+            });
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
